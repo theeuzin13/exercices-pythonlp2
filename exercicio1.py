f015c07623adf8c00d3b8c69a11967da2b94d1b1
Matheus <matt.henrique13700@gmail.com>
2025-09-26 22:23:05 -0300

    feat:exercise1

--- exercicio1.py
+++ exercicio1.py
@@ -7,7 +7,7 @@ class Aluno:
 
     @property
     def nome(self):
-        return self.nome
+        return self.__nome
 
     @nome.setter
     def nome(self, nome):
@@ -15,7 +15,7 @@ class Aluno:
 
     @property
     def idade(self):
-        return self.idade
+        return self.__idade
 
     @idade.setter
     def idade(self, idade):
@@ -23,8 +23,21 @@ class Aluno:
 
     @property
     def nota(self):
-        return self.nota
+        return self.__nota
 
     @nota.setter
-    def cpf(self, nota):
+    def nota(self, nota):
         self.__nota = nota
+
+if __name__ == '__main__':
+    al1 = Aluno('Gabriel', 22, 7.3)
+    al2 = Aluno('Isabela', 16, 10.0)
+
+    print(f'Aluno 1: {al1.nome}, Idade: {al1.idade}, Nota: {al1.nota}')
+    print(f'Aluno 2: {al2.nome}, Idade: {al2.idade}, Nota: {al2.nota}')
+
+    # Alterando atributos com os setters
+    al1.nome = 'Gabriel Santos'
+    al1.nota = 8.5
+
+    print(f'Aluno 1 (atualizado): {al1.nome}, Idade: {al1.idade}, Nota: {al1.nota}')
